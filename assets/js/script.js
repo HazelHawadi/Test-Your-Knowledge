9040bae007c8175d2332f1f9233eef72de6d2100
Hazel Hawadi <hawadihazel@icloud.com>
2024-06-08 02:12:23 +0000

    add variables to keep the current state of the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+//variables to keep track of the current state of the quiz
+let questionIndex = 0;
+let score = 0;
+let quizEnded = false;
+
+// DOM elements
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons'); 
+const nextButton = document.getElementById('next-btn'); 
+const resultArea = document.getElementById('result-area');
+const resultElement = document.getElementById('result'); 
+const restartButton = document.getElementById('restart-btn');
+const progressElement = document.getElementById('progress');
+
+
 /**Function to start the quiz */
 function startQuiz() {
    
