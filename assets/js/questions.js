ad1b45e86cda8cce981b0084a473f0e048574b82
Hazel Hawadi <hawadihazel@icloud.com>
2024-06-08 02:06:24 +0000

    add questions for the quiz in own file with four different answer choices and correct answers

---
+++ assets/js/questions.js
@@ -0,0 +1,72 @@
+const questions = [
+    {
+        question: 'What is the correct syntax to create a new object in JavaScript?',
+        answers: ['var obj = [];', 'var obj = new Array();', 'var obj = {};', 'var obj = Object[];'],
+        correctAnswer: 'var obj = {};'
+    },
+    {
+        question: 'Which method can be used to find the length of a string?',
+        answers: ['length()', 'length', 'len()', 'size()'],
+        correctAnswer: 'length'
+    },
+    {
+        question: 'How do you write an arrow function in JavaScript?',
+        answers: ['function myFunction() => {}', 'myFunction() => {}', 'var myFunction = function() {}', 'const myFunction = () => {}'],
+        correctAnswer: 'const myFunction = () => {}'
+    },
+    {
+        question: 'What is the output of the following code: "console.log(typeof null);"?',
+        answers: ['null', 'object', 'undefined', 'number'],
+        correctAnswer: 'object'
+    },
+    {
+        question: 'Which of the following is a correct way to create an array in JavaScript?',
+        answers: ['var arr = array(1, 2, 3);', 'var arr = [1, 2, 3];', 'var arr = (1, 2, 3);', 'var arr = {1, 2, 3};'],
+        correctAnswer: 'var arr = [1, 2, 3];'
+    },
+    {
+        question: 'What does HTML stand for?',
+        answers: ['Hyper Text Markup Language', 'Hyper Trainer Marking Language', 'Hyper Text Marketing Language', 'Hyper Text Markup Leveler'],
+        correctAnswer: 'Hyper Text Markup Language'
+    },
+    {
+        question: 'Which HTML tag is used to define an internal style sheet?',
+        answers: ['<script>', '<css>', '<style>', '<link>'],
+        correctAnswer: '<style>'
+    },
+    {
+        question: 'How can you create a hyperlink in HTML?',
+        answers: ['<a link="url">link text</a>', '<link href="url">link text</link>', '<a url="url">link text</a>', '<a href="url">link text</a>'],
+        correctAnswer: '<a href="url">link text</a>'
+    },
+    {
+        question: 'Which HTML element is used to specify a footer for a document or section?',
+        answers: ['<footer>', '<bottom>', '<section>', '<foot>'],
+        correctAnswer: '<footer>'
+    },
+    {
+        question: 'What is the purpose of the "<head>" element in HTML?',
+        answers: ['To include the main content of the document', 'To create a header for the document', 'To contain metadata about the document', 'To specify the footer of the document'],
+        correctAnswer: 'To contain metadata about the document'
+    },
+    {
+        question: 'What does CSS stand for?',
+        answers: ['Cascading Style Sheets', 'Colorful Style Sheets', 'Creative Style Sheets', 'Computer Style Sheets'],
+        correctAnswer: 'Cascading Style Sheets'
+    },
+    {
+        question: 'How do you add a background color in CSS?',
+        answers: ['color: red;', 'background-color: red;', 'bg-color: red;', 'background: red;'],
+        correctAnswer: 'background-color: red;'
+    },
+    {
+        question: 'Which property is used to change the text color of an element?',
+        answers: ['text-color', 'fgcolor', 'color', 'font-color'],
+        correctAnswer: 'color'
+    },
+    {
+        question: 'How do you select an element with id "demo" in CSS?',
+        answers: ['.demo', '*demo', '#demo', 'demo'],
+        correctAnswer: '#demo'
+    }
+];
