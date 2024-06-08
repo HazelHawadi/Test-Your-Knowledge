49b6a3148ad88d1965f29ae37e4337832fd0eb4c
Hazel Hawadi <hawadihazel@icloud.com>
2024-06-08 02:09:07 +0000

    create empty functions needed for the quiz page

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+/**Function to start the quiz */
+function startQuiz() {
+   
+}
+
+/**Function to display the current question and answer choices */
+function showQuestion() {
+
+}
+
+/**Function to handle the answer selection */
+function selectAnswer() {
+    
+}
+
+/**Function to set if the answer selected is correct or wrong */ 
+function correctWrong() {
+    
+}
+
+/**Function to display the result */
+function showResult() {
+    
+}
+
+/**Event listener for the next button */
+nextButton.addEventListener(
+
+);
+
